a72be9450e51cf2f10c21444d2e5755837f18962
ekdlsdlsep <dain7362@gmail.com>
2024-05-06 16:56:54 +0900

    commit

--- app/src/main/java/com/example/gilin/MainActivity.java
+++ app/src/main/java/com/example/gilin/MainActivity.java
@@ -1,24 +1,82 @@
 package com.example.gilin;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.gilin.dain.Login;
+import com.example.gilin.dain.Util.PreferenceUtil;
+import com.example.gilin.hani.mypage;
+import com.example.gilin.kst.Map;
+import com.example.gilin.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView bottomNavigationView = binding.bottomNavigation;
+
+        String loginStatus = PreferenceUtil.getLoginStatus(this);
+        String userId = PreferenceUtil.getUserId(this);
+        Log.d("MyActivity", "Login status: " + loginStatus);
+        Log.d("MyActivity", "Login status: " + userId);
+
+        if ("x".equals(loginStatus)) {
+            transferTo(new Login());
+        } else {
+            transferTo(new Map());
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.myPage) {
+                    transferTo(new mypage());
+                    return true;
+                }
+//                else if (item.getItemId() == R.id.safeBall) {
+//                    transferTo(new SafeBall());
+//                    return true;
+//                } else if (item.getItemId() == R.id.user) {
+//                    transferTo(new Login());
+//                    return true;
+//                } else if (item.getItemId() == R.id.bookMark) {
+//                    transferTo(new BookMark());
+//                    return true;
+//                } else if (item.getItemId() == R.id.report) {
+//                    transferTo(new Report());
+//                    return true;
+//                }
+                return false;
+            }
         });
+    }
+
+    // 네비게이션 숨기는 함수
+    public void hideBottomNav() {
+        binding.bottomNavigation.setVisibility(View.GONE);
+    }
+
+    public void showBottomNav() {
+        binding.bottomNavigation.setVisibility(View.VISIBLE);
+    }
+
+    void transferTo(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
---
+++ app/src/main/java/com/example/gilin/kst/Map.java
@@ -0,0 +1,86 @@
+package com.example.gilin.kst;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.gilin.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Map extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        LatLng latLng = new LatLng(35.1639211, 126.7975209);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("광주여자대학교");
+        googleMap.addMarker(markerOptions);
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            checkLocationPermissionWithRationale();
+        }
+    }
+
+    private void checkLocationPermissionWithRationale() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("위치정보")
+                        .setMessage("이 앱을 사용하기 위해서는 위치정보에 접근이 필요합니다. 위치정보 접근을 허용하여 주세요.")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        }).create().show();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    googleMap.setMyLocationEnabled(true);
+                }
+            } else {
+                Toast.makeText(getContext(), "permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
